454ec644483e7753732f67041dc3872d27f43188
sebahattin <sebkucuk05@gmail.com>
2023-02-05 18:57:39 +0300

    v2

---
+++ src/Dice.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './Dice.css'
+
+class Dice extends Component {
+	render() {
+		return (
+			// <i className="fas fa-8x fa-dice-one"></i>
+			<i className=`` ></i >
+		)
+	}
+}
+export default Dice
+
+
+// import "./Dice.css"
+
+// function Dice() {
+// 	return (
+// 		<div className='dicecomponent'>
+// 			✅This Component Dice.jsx
+// 		</div>
+// 	)
+// }
+
+// export default Dice
+
